327cb4f29c020e6dba975d9860c4cf15e618c583
Mostafa <Mostafa@DESKTOP-FATN97D>
2026-03-01 17:49:14 +0200

    Add project files.

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string CustomerName { get; set; } = null!;
+        public string CustomerEmail { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int TotalItems { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal FinalTotal { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new();
+    }
+
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int AvailableQuantity { get; set; }
+    }
+
+}
